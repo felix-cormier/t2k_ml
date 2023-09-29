b19f12fdb728de654591f41c273963d7f1e18846
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-09-29 08:02:18 -0700

    Update infer_dwall.py

--- infer_dwall.py
+++ infer_dwall.py
@@ -1,18 +1,15 @@
 import h5py
 import numpy as np
-import glob
 import os
-import math
-
-from generics_python.make_plots import generic_histogram, generic_2D_plot
 from plot_wcsim import calculate_wcsim_wall_variables, load_geofile, convert_values, convert_label, get_cherenkov_threshold
 import matplotlib.pyplot as plt
 
-def plot_walls(input_path, text_file=True):
-    """Plots PMT and event variables
+def plot_walls(input_path, output_dir, text_file=True):
+    """Plots wall and towall vs num_pmts
 
     Args:
-        input_path (_type_): Path to either .hy file or text file with multiple paths and names of .hy files
+        input_path (str): Path to either .hy file or text file with multiple paths and names of .hy files
+        output_dir (str): Path to directory where plots should be saved to.
         text_file (bool, optional): If the input is in text file or string to single .hy file. Defaults to False.
     """
     file_paths=input_path
@@ -74,21 +71,20 @@ def plot_walls(input_path, text_file=True):
         towall.append(temp_towall)
         num_pmt.append(temp_num_pmt)
 
-    # seperate these out by label in e and m
-    # don't calculate the stuff you don't need
-    # + go back to normal input plots
-
+    # create two figrues
     fig1 = plt.figure()
     ax1 = fig1.gca()
 
     fig2 = plt.figure()
     ax2 = fig2.gca()
 
+    # make scatter plot of wall and towall vs num_pmts for electrons and muons
     for wall_i, towall_i, num_pmt_i, label_i in zip(wall, towall, num_pmt, label):
         str_label = convert_label(np.median(label_i))
         ax1.scatter(wall_i, num_pmt_i, alpha=0.2, label=str_label, s=2)
-        ax2.scatter(towall, num_pmt, alpha=0.2, label=label, s=2)
+        ax2.scatter(towall_i, num_pmt, alpha=0.2, label=label, s=2)
 
+    # add labels and stuff to make plots more understandable
     ax1.vlines(200, ymin=0, ymax=max(num_pmt[0]), linestyles='--', label='2 m')
     ax1.set_xlabel('Wall [cm]')
     ax1.set_ylabel('Number of PMTs')
@@ -99,8 +95,9 @@ def plot_walls(input_path, text_file=True):
     ax2.set_ylabel('Number of PMTs')
     ax2.legend()
         
-    fig2.savefig('/fast_scratch_2/aferreira/t2k/ml/analysis_plots/towall_v_pmts.png')
-    fig1.savefig('/fast_scratch_2/aferreira/t2k/ml/analysis_plots/wall_v_pmts.png')
+    # save
+    fig2.savefig(output_dir+'towall_v_pmts.png')
+    fig1.savefig(output_dir+'wall_v_pmts.png')
 
-
-plot_walls('plotting_paths.txt')
+# run the function
+plot_walls(input_path='plotting_paths.txt', output_dir='/fast_scratch_2/aferreira/t2k/ml/analysis_plots/')
