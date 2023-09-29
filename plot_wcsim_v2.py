85c590e4fa92b7e3e6334e90afd87f9a4f4f46b0
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-09-29 13:53:14 -0700

    Update plot_wcsim_v2.py

--- plot_wcsim_v2.py
+++ plot_wcsim_v2.py
@@ -520,8 +520,9 @@ def plot_skdetsim(input_path, output_path, text_file=False, moreVariables = Fals
         
         #with h5py.File(path+'/combine_combine.hy',mode='r') as h5fw:
         with h5py.File(path+'/digi_combine.hy',mode='r') as h5fw:
-
-            h5fw = h5fw[h5fw['keep_event']==True]
+            keys = h5fw.keys() 
+            for k in keys:
+                h5fw[k] = h5fw[k][h5fw['keep_event']==True]
 
             #Temporary list of variables for each event
             temp_mean_charge = []
