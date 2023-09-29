e53cdfb3539eb85ba77f9a9a1b083735f6c450b8
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-09-28 20:07:42 -0700

    Update balance_hdf5.py

--- balance_hdf5.py
+++ balance_hdf5.py
@@ -118,7 +118,7 @@ def sample_lowest_min_energy(input_path, output_path=None, text_file=False):
                 ## THIS MEHTOD ASUMES THE FILE IS ALWAYS TRAVERSED IN THE SAME 
                 ## WAY BUT I WILL TEST THIS LATER ON
                 keys = h5fw.keys()
-                with h5py.File(output_path+f'/digi_combine_balanced_{j}.hy', "w") as new_h5fw:
+                with h5py.File(output_path+f'/digi_combine_balanced2_{j}.hy', 'w') as new_h5fw:
                     for k in tqdm(keys):
                         new_h5fw[k] = h5fw[k][new_indicies_to_save[j]]
 
