5067967427339d8f54ed68ecba4a114d2ed2e5a2
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-11-16 21:01:07 -0800

    Update make_visualizations.py

--- make_visualizations.py
+++ make_visualizations.py
@@ -112,6 +112,7 @@ def make_visualizations_specific(input_path, output_path=None, towall_bounds=(0,
     Returns:
         None
     """
+    import sys
     print(sys.path)
     h5_file = h5.File(input_path,'r')
     geofile = load_geofile('data/geofile.npz')
